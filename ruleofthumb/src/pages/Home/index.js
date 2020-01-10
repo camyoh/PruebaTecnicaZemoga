1b8711a4ab96b8b08f2e3a9ffc08250dbab68371
Carlos Andres Mendieta <carlos.camyoh@gmail.com>
2020-01-09 19:07:51 -0500

    changing the font to Lato, creading header component

--- ruleofthumb/src/pages/Home/index.js
+++ ruleofthumb/src/pages/Home/index.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './styles.css';
+import {Header} from '../../components'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <Header />
     );
 }
 
